02dd7ce7d543c3310648a1eb0a11bc6f800b50cf
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2020-07-14 17:31:47 -0400

    Add logging

--- scripts/archr2adata.py
+++ scripts/archr2adata.py
@@ -2,74 +2,96 @@
 # coding: utf-8
 
 import os
+import sys
+import argparse
 import pandas as pd
 import scanpy as sc
 import scipy.io
+import logging
+
+
+logger = logging.getLogger("archr2adata")
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("archr2adata.log"),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
+
 
 def make_adata(path_data):
 
     # load
+    logger.info("Reading barcodes...")
     barcodes = pd.read_csv(
-        os.path.join(path_data, "peak_counts/cells.csv.gz"),
-        skiprows=[0], header=None
+        os.path.join(path_data, "peak_counts/cells.csv.gz"), skiprows=[0], header=None
     )[1]
 
+    logger.info("Reading peaks...")
     peaks = pd.read_csv(
-        os.path.join(path_data, "peak_counts/peaks.csv.gz"),
-        index_col=0
+        os.path.join(path_data, "peak_counts/peaks.csv.gz"), index_col=0
     )
 
-    counts = scipy.io.mmread(
-        os.path.join(path_data, "peak_counts/counts.mtx.gz")
-    )
+    logger.info("Reading counts...")
+    counts = scipy.io.mmread(os.path.join(path_data, "peak_counts/counts.mtx.gz"))
 
     # construct AnnData
+    logger.info("Constructing AnnData...")
     adata = sc.AnnData(counts.T)
 
     adata.obs_names = barcodes
     adata.var_names = peaks.index
 
-    for col in ['distToGeneStart', 'nearestGene', 'peakType', 'distToTSS', 'nearestTSS','replicateScoreQuantile']:
+    for col in [
+        "distToGeneStart",
+        "nearestGene",
+        "peakType",
+        "distToTSS",
+        "nearestTSS",
+        "replicateScoreQuantile",
+    ]:
         adata.var[col] = peaks[col]
 
     # SVD
+    logger.info("Adding SVD...")
     adata.obsm["X_svd"] = pd.read_csv(
-        os.path.join(path_data, "svd.csv"),
-        index_col=0
+        os.path.join(path_data, "svd.csv"), index_col=0
     ).values
 
     # UMAP
+    logger.info("Adding UMAP...")
     adata.obsm["umap"] = pd.read_csv(
-        os.path.join(path_data, "umap.csv"),
-        index_col=0
+        os.path.join(path_data, "umap.csv"), index_col=0
     ).values
 
     # metadata
-    df_meta = pd.read_csv(
-        os.path.join(path_data, "cell_metadata.csv"),
-        index_col=0
-    )
+    logger.info("Adding metadata...")
+    df_meta = pd.read_csv(os.path.join(path_data, "cell_metadata.csv"), index_col=0)
 
     for col in df_meta.columns:
         adata.obs[col] = df_meta[col].values
 
     # gene scores
+    logger.info("Adding gene scores...")
     df_gene_scores = pd.read_csv(
-        os.path.join(path_data, "gene_scores.csv.gz"),
-        index_col=0
+        os.path.join(path_data, "gene_scores.csv.gz"), index_col=0
     ).T
 
-    adata.obsm['GeneScores'] = df_gene_scores.values
-    adata.uns['GeneScoresColumns'] = df_gene_scores.columns.values
+    adata.obsm["GeneScores"] = df_gene_scores.values
+    adata.uns["GeneScoresColumns"] = df_gene_scores.columns.values
 
     # a writer for type <class 'scipy.sparse.coo.coo_matrix'> has not been implemented yet.
     from scipy.sparse import csr_matrix
 
     adata.X = csr_matrix(adata.X)
 
-    adata.write(
-        os.path.join(path_data, "preprocessed.h5ad")
-    )
+    logger.info(adata)
+
+    logger.info("Writing to preprocessed.h5ad...")
+    adata.write(os.path.join(path_data, "preprocessed.h5ad"))
 
 
 def parse_arguments():
@@ -81,7 +103,7 @@ def parse_arguments():
         action="store",
         dest="path_data",
         help="path to data files (*.csv, *.mtx, ...)",
-        required=True
+        required=True,
     )
 
     # parse arguments
@@ -94,4 +116,8 @@ if __name__ == "__main__":
 
     params = parse_arguments()
 
+    logger.info("Starting...")
+
     make_adata(params.path_data)
+
+    logger.info("DONE.")
